a04bea189fedbe683e09e688787516a85bfbaf3c
Akshay <52096313+AkshayGadepally@users.noreply.github.com>
2025-04-06 19:17:44 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,31 @@
+package practice;
+
+public class Student {
+	private String name;
+	private int    rollno;
+	private char   grade;
+	
+	public Student(String name,int rollno,char grade) {
+		this.name = name;
+		this.rollno = rollno;
+		this.grade = grade;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getRollno(){
+		return rollno;
+	}
+	public void setRollno(int rollno){
+		this.rollno=rollno;
+	}
+	public int getGrade(){
+		return grade;
+	}
+	public void setGrade(char grade) {
+		this.grade = grade;
+	}
+}

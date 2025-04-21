002115cb829c5f439e4e3ca5f22a914bb4034ef4
Akshay <52096313+AkshayGadepally@users.noreply.github.com>
2025-04-21 12:49:08 +0530

    Add files via upload

--- StudentServiceImpl.java
+++ StudentServiceImpl.java
@@ -7,6 +7,26 @@ public class StudentServiceImpl implements StudentService{
 	@Override
 	public Boolean addStudent(Student student) {
 		StudentDAO dao = new StudentDAOImpl();
+		if(student == null) {
+			throw new IllegalArgumentException("Student object cannot be null.");
+		}
+		if(student.getName() == null || student.getName().trim().isEmpty()) {
+			throw new IllegalArgumentException("Student name cannot be null.");
+		}
+		if(student.getRollno() <=0 ) {
+			throw new IllegalArgumentException("Student Roll Number cannot be be less than or equal to 0.");
+		}
+		Character grade = student.getGrade();
+
+		if (grade == null) {
+		    throw new IllegalArgumentException("Grade cannot be null.");
+		}
+
+		grade = Character.toUpperCase(grade);
+
+		if (grade != 'A' && grade != 'B' && grade != 'C' && grade != 'D') {
+		    throw new IllegalArgumentException("Grade must be A, B, C, or D.");
+		}
 		return dao.addStudent(student);
 	}
 
@@ -25,6 +45,19 @@ public class StudentServiceImpl implements StudentService{
 	@Override
 	public Boolean updateStudent(Student student) {
 		StudentDAO dao = new StudentDAOImpl();
+		if(student == null) {
+			throw new IllegalArgumentException("Student object cannot be null.");
+		}
+		if(student.getName() == null || student.getName().trim().isEmpty()) {
+			throw new IllegalArgumentException("Student name cannot be null.");
+		}
+		if(student.getRollno() <=0 ) {
+			throw new IllegalArgumentException("Student Roll Number cannot be be less than or equal to 0.");
+		}
+		char grade = Character.toUpperCase(student.getGrade());
+		if(grade != 'A' && grade != 'B' && grade != 'C' && grade != 'D') {
+			throw new IllegalArgumentException("Grade must be A, B, C, or D.");
+		}
 		return dao.updateStudent(student);
 	}
 
